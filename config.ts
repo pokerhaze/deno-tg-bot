06da3db5667a923a65dfc19bd9bf887d01b7ffb6
Kung-Chiang Cheng <kungchiang.cheng@shopee.com>
2024-10-26 16:32:30 +0800

    8000 is allow port.

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
     token: Deno.env.get("BOT_TOKEN") || "",
-    port: parseInt(Deno.env.get("PORT") || "8090"),
+    port: parseInt(Deno.env.get("PORT") || "8000"),
 };
 
 if (!config.token) {
